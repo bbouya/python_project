511c874ecb768397870ddb508e33c10b011c7015
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-11-03 14:07:14 +0100

    files

--- MyBlogProfil/article/urls.py
+++ MyBlogProfil/article/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
+from . import views
 
 app_name = 'article'
 
 urlpatterns = [
     #include the different patern in here
 
+    path('article_list/', views.article_list, name='article_list'),
     
 ]
