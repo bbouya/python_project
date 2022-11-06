22897b5bbfce46fa2d7e94af352d5cc876c66103
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-11-06 21:18:03 +0100

    python

---
+++ coffe_programm/oop_coffe/menuItem.py
@@ -0,0 +1,7 @@
+
+# Create a class Menu item class
+class MenuItem:
+    def __init__(self,name , cost, ingredients):
+        self.name = name 
+        self.cost = cost 
+        self.ingredients = ingredients
